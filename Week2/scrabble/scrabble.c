13ddb7feaed5df26ebe9e222ec21bc1b62518619
ronny <42517731@qq.com>
2024-11-20 01:00:43 +0800

    使用数组算法修改

--- Week2/scrabble/scrabble.c
+++ Week2/scrabble/scrabble.c
@@ -1,21 +1,23 @@
-/*
-* File: example.c
-* Author: name <yourmail@gmail.com>
-* Copyright (c) 2024, name
-* Public Domain.
-* Version: 1.0
-*
-* Description:
-* 拼字游戏，根据两位玩家输入的单词来计算得分，分高获胜
-* check50 cs50/problems/2024/x/scrabble
-* style50 scrabble.c
-* submit50 cs50/problems/2024/x/scrabble
-*/
+/**
+ * File: scrabble.c
+    * Author: name <yourmail@gmail.com>
+ * Copyright (c) 2024, name
+ * Public Domain.
+ * Version: 1.1
+ *
+ * Description:
+ * 拼字游戏，根据两位玩家输入的单词来计算得分，分高获胜
+ * check50 cs50/problems/2024/x/scrabble
+ * style50 scrabble.c
+ * submit50 cs50/problems/2024/x/scrabble
+ */
 
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 string str_toupper(string s);
 int score_word(string s);
@@ -26,24 +28,24 @@ int main(void)
     string player1 = get_string("Player 1: ");
     string player2 = get_string("Player 2: ");
 
-    // 将单词转化为得分，并比较分数
-    int player1_score = score_word(str_toupper(player1));
-    int player2_score = score_word(str_toupper(player2));
+    int player1_score = score_word(player1);
+    int player2_score = score_word(player2);
 
     // 判断获胜者
     if (player1_score > player2_score)
     {
-        printf("Player 1 wins!");
+        printf("Player 1 wins!\n");
     }
     else if (player1_score < player2_score)
     {
-        printf("Player 2 wins!");
+        printf("Player 2 wins!\n");
     }
     else
     {
-        printf("Tie!");
+        printf("Tie!\n");
     }
-	return 0;
+
+    return 0;
 }
 
 // 根据单词统计得分
@@ -53,126 +55,14 @@ int score_word(string s)
 
     for (int i = 0, n = strlen(s); i <= n; i++)
     {
-        if (s[i] == 'A')
+        char word = toupper(s[i]);
+
+        if (word > 64 && word < 91)
         {
-            score += 1;
-        }
-        else if (s[i] == 'B')
-        {
-            score += 3;
-        }
-        else if (s[i] == 'C')
-        {
-            score += 3;
-        }
-        else if (s[i] == 'D')
-        {
-            score += 2;
-        }
-        else if (s[i] == 'E')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'F')
-        {
-            score += 4;
-        }
-        else if (s[i] == 'G')
-        {
-            score += 2;
-        }
-        else if (s[i] == 'H')
-        {
-            score += 4;
-        }
-        else if (s[i] == 'I')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'J')
-        {
-            score += 8;
-        }
-        else if (s[i] == 'K')
-        {
-            score += 5;
-        }
-        else if (s[i] == 'L')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'M')
-        {
-            score += 3;
-        }
-        else if (s[i] == 'N')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'O')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'P')
-        {
-            score += 3;
-        }
-        else if (s[i] == 'Q')
-        {
-            score += 10;
-        }
-        else if (s[i] == 'R')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'S')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'T')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'U')
-        {
-            score += 1;
-        }
-        else if (s[i] == 'V')
-        {
-            score += 4;
-        }
-        else if (s[i] == 'W')
-        {
-            score += 4;
-        }
-        else if (s[i] == 'X')
-        {
-            score += 8;
-        }
-        else if (s[i] == 'Y')
-        {
-            score += 4;
-        }
-        else if (s[i] == 'Z')
-        {
-            score += 10;
-        }
-        else
-        {
-            score += 0;
+            score += POINTS[word - 'A'];
         }
     }
 
     return score;
 }
 
-// 将输入的字符串统一转变为大写
-string str_toupper(string s)
-{
-    for (int i = 0, n = strlen(s); i < n; i++)
-    {
-        s[i] = toupper(s[i]);
-    }
-
-    return s;
-}
