56ebac81bee0578a15542a07e3a6f7da1cddde89
ronny <42517731@qq.com>
2024-11-25 19:21:56 +0800

    add tideman

---
+++ Week3/plurality/test.c
@@ -0,0 +1,18 @@
+/**
+ * File: example.c
+ * Author: name <yourmail@gmail.com>
+ * Copyright (c) 2024, name
+ * Public Domain.
+ * Version: 1.0
+ *
+ * Description:
+ * 
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", 7/2);
+}
