f301a945be7a7e1af766a86d8d93caad5e05aad8
Ronny <yq.worm@gmail.com>
2024-12-03 06:31:24 +0000

    Tue, Dec 3, 2024, 6:31 AM +00:00

--- Week5/inheritance/inheritance.c
+++ Week5/inheritance/inheritance.c
@@ -50,7 +50,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        new_person -> parent[0]
+        new_person -> parent[0] = parrent0;
+        new_person -> parent[1] = parrent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
     }
