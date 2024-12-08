0947f6d7a611065f3f4ecd45df6daa1937dc87f6
ronny <yq.worm@gmail.com>
2024-12-08 22:51:00 +0000

    Invalid DateTime

--- Week7/movies/11.sql
+++ Week7/movies/11.sql
@@ -1,7 +1,8 @@
 SELECT title FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
 JOIN stars ON movies.id = stars.id
-JOIN people WHERE people.name = 'Chadwick Boseman'
+JOIN people ON stars.person_id = people.id
+WHERE people.name = 'Chadwick Boseman'
 ORDER BY rating
 LIMIT 5;
 
