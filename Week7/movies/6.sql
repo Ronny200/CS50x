7f38b881a0496eff9e25ebdc6c4c0aa9802dadfd
ronny <yq.worm@gmail.com>
2024-12-08 21:09:26 +0000

    Invalid DateTime

--- Week7/movies/6.sql
+++ Week7/movies/6.sql
@@ -3,7 +3,7 @@ JOIN movies ON ratings.movie_id = movies.id
 WHERE movies.year = 2012;
 
 SELECT AVG(rating) FROM ratings
-WHERE(
-    SELECT year FROM movies
+WHERE movie_id IN (
+    SELECT id FROM movies
     WHERE year = 2012
 );
