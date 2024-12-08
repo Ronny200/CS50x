881f421b6d3c3a1bad0141b822d6da4f6092504f
ronny <yq.worm@gmail.com>
2024-12-08 21:46:40 +0000

    Invalid DateTime

--- Week7/movies/9.sql
+++ Week7/movies/9.sql
@@ -1,2 +1,5 @@
-SELECT name FROM people
-JOIN 
+SELECT name,year FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.year = 2004
+ORDER BY birth;
