a82cf61ffee33350577d4971387de041f09d729f
ronny <yq.worm@gmail.com>
2024-12-08 23:28:08 +0000

    Invalid DateTime

--- Week7/movies/12.sql
+++ Week7/movies/12.sql
@@ -16,4 +16,4 @@ JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE people.name in ('Jennifer Lawrence', 'Bradley Cooper')
 GROUP BY movies.title
-HAVING COUNT(DISTINCT people.name = 2);
+HAVING COUNT(DISTINCT people.name) = 2;
