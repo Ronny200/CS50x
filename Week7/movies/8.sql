3ed6eb401bed6394e54242af5c82e636099c14bf
ronny <yq.worm@gmail.com>
2024-12-08 21:41:37 +0000

    Invalid DateTime

--- Week7/movies/8.sql
+++ Week7/movies/8.sql
@@ -1,13 +1,11 @@
-SELECT name FROM people
-WHERE id IN (
-    SELECT person_id FROM stars
-    JOIN movies ON stars.movie_id = movies.id
-    WHERE movies.title = 'Toy Story'
-);
+-- SELECT name FROM people
+-- WHERE id IN (
+--     SELECT person_id FROM stars
+--     JOIN movies ON stars.movie_id = movies.id
+--     WHERE movies.title = 'Toy Story'
+-- );
 
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.title = 'Toy Story';
-
-
