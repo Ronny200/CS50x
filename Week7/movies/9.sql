7397eee27c61815cb5c2b27cc1b170665afb563d
ronny <yq.worm@gmail.com>
2024-12-09 05:49:51 +0000

    Invalid DateTime

--- Week7/movies/9.sql
+++ Week7/movies/9.sql
@@ -6,3 +6,7 @@ ORDER BY birth;
 
 包含 1 列和 19,325 行的表
 
+SELECT COUNT(name) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.year =2004;
