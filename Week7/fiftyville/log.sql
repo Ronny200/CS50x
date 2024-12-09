6e93a101ed497f4862809d1be8d90efedb22a1e4
ronny <yq.worm@gmail.com>
2024-12-09 07:21:11 +0000

    Invalid DateTime

--- Week7/fiftyville/log.sql
+++ Week7/fiftyville/log.sql
@@ -1,12 +1,28 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- 查看犯罪现场报告的结构
+-- 根据结构查看2023年7月28日Humphrey街的记录
 .schema crime_scene_reports
 
--- 根据结构查看2023年7月28日Humphrey街的记录
 SELECT description FROM crime_scene_reports
 WHERE year = 2023 AND month = 7 AND day = 28
 AND street = 'Humphrey Street';
+-- CS50 鸭子被盗事件发生在上午 10:15
+-- 事发地点位于 Humphrey Street 面包店
+-- 今天采访了当时在场的三名目击者——每人的采访记录都提到了这家面包店
+-- 乱扔垃圾事件发生在 16:36。没有已知目击者
 
 -- 根据现场报告查看面包店的信息
 .scheme bakery_security_logs
+CREATE TABLE bakery_security_logs (
+    id INTEGER,
+    year INTEGER,
+    month INTEGER,
+    day INTEGER,
+    hour INTEGER,
+    minute INTEGER,
+    activity TEXT,
+    license_plate TEXT,
+    PRIMARY KEY(id)
+);
+SELECT activity FROM bakery_security_logs
+WHERE 
