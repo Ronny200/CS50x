fd7702361b5c69b3a48058394f25fe789a41db37
ronny <yq.worm@gmail.com>
2024-12-09 07:42:34 +0000

    Invalid DateTime

--- Week7/fiftyville/log.sql
+++ Week7/fiftyville/log.sql
@@ -24,14 +24,5 @@ CREATE TABLE bakery_security_logs (
 SELECT hour, minute, activity, license_plate FROM bakery_security_logs
 WHERE day = 28 AND hour < 11;
 
--- 怀疑对象为10：00-10:23出门的人
--- | 10   | 16     | exit     | 5P2BI95       |
--- | 10   | 18     | exit     | 94KL13X       |
--- | 10   | 18     | exit     | 6P58WS2       |
--- | 10   | 19     | exit     | 4328GD8       |
--- | 10   | 20     | exit     | G412CB7       |
--- | 10   | 21     | exit     | L93JTIZ       |
--- | 10   | 23     | exit     | 322W7JE       |
--- | 10   | 23     | exit     | 0NTHK55       |
--- | 10   | 35     | exit     | 1106N58
+
 
