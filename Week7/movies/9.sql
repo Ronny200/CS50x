3250b9bfe7264e813268eda60fbbc43add4a28d6
ronny <yq.worm@gmail.com>
2024-12-09 06:03:33 +0000

    Invalid DateTime

--- Week7/movies/9.sql
+++ Week7/movies/9.sql
@@ -10,5 +10,5 @@ SELECT COUNT(name) FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year =2004
-GROUP BY people.id
+GROUP BY people.id, people.name
 ORDER BY birth;
