787d00a76838033c8f4f28de6a605453c6e64143
ronny <yq.worm@gmail.com>
2024-12-09 08:34:01 +0000

    Invalid DateTime

--- Week7/fiftyville/log.sql
+++ Week7/fiftyville/log.sql
@@ -29,6 +29,8 @@ WHERE day = 28 AND hour > 9 AND hour < 11 AND minute < 15;
 SELECT name, phone_number,passport_number  FROM people
 JOIN bakery_security_logs
 ON people.license_plate = bakery_security_logs.license_plate
+JOIN phone_calls
+ON phone_calls.
 WHERE bakery_security_logs.day = 28;
 
-SELECT * FROM interviews;
+SELECT * FROM phone_calls;
