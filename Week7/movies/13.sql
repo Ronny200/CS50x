3c2efc7d1e8ee632fb7faec467a530a2ae6d063b
ronny <yq.worm@gmail.com>
2024-12-09 04:47:32 +0000

    Invalid DateTime

--- Week7/movies/13.sql
+++ Week7/movies/13.sql
@@ -1,5 +1,5 @@
-SELECT person_id FROM stars WHERE 
-SELECT movie_id FROM stars WHERE person_id=
+SELECT COUNT(*) FROM stars WHERE movie_id =
+(SELECT movie_id FROM stars WHERE person_id=
     (SELECT id FROM people
-     WHERE name = 'Kevin Bacon' AND birth = 1958);
+     WHERE name = 'Kevin Bacon' AND birth = 1958));
 
