41bea50462a7362f15afdd0f05c03f07378fcccb
ronny <yq.worm@gmail.com>
2024-12-09 07:42:00 +0000

    Invalid DateTime

--- Week7/fiftyville/log.sql
+++ Week7/fiftyville/log.sql
@@ -6,10 +6,7 @@
 SELECT description FROM crime_scene_reports
 WHERE year = 2023 AND month = 7 AND day = 28
 AND street = 'Humphrey Street';
--- CS50 鸭子被盗事件发生在上午 10:15
--- 事发地点位于 Humphrey Street 面包店
--- 今天采访了当时在场的三名目击者——每人的采访记录都提到了这家面包店
--- 乱扔垃圾事件发生在 16:36。没有已知目击者
+
 
 -- 根据现场报告查看面包店的信息
 .scheme bakery_security_logs
