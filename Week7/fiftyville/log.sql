b4e5190129754a0b0042b80165b6b58c8452e0af
ronny <yq.worm@gmail.com>
2024-12-09 12:10:32 +0000

    Invalid DateTime

--- Week7/fiftyville/log.sql
+++ Week7/fiftyville/log.sql
@@ -73,21 +73,20 @@ WHERE
 -- 根据所有线索先追踪小偷
 SELECT DISTINCT people.name, airports.city FROM people
 JOIN airports
-    ON flights.destination_airport_id = airports.id
+    ON airports.id = flights.destination_airport_id
 JOIN flights
-    ON passengers.flight_id = flights.id
+    ON flights.id = passengers.flight_id
 JOIN passengers
-    ON people.passport_number = passengers.passport_number
-
-
-JOIN bakery_security_logs
-    ON people.license_plate = bakery_security_logs.license_plate
-JOIN phone_calls
-    ON people.phone_number = phone_calls.caller
+    ON passengers.passport_number = people.passport_number
 JOIN bank_accounts
     ON people.id = bank_accounts.person_id
 JOIN atm_transactions
     ON bank_accounts.account_number = atm_transactions.account_number
+JOIN bakery_security_logs
+    ON people.license_plate = bakery_security_logs.license_plate
+JOIN phone_calls
+    ON people.phone_number = phone_calls.caller
+
 
 WHERE
     atm_transactions.month = 7
