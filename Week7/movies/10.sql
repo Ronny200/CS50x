ddf8352d6a623f07eb9e9ac3b0b1355503dadd90
ronny <yq.worm@gmail.com>
2024-12-09 06:08:33 +0000

    Invalid DateTime

--- Week7/movies/10.sql
+++ Week7/movies/10.sql
@@ -1,8 +1,6 @@
-SELECT COUNT(DISTINCT name) FROM people
+SELECT name FROM people
 JOIN directors ON people.id = directors.person_id
 JOIN movies ON directors.movie_id = movies.id
 JOIN ratings ON movies.id = ratings.movie_id
-WHERE ratings.rating >= 9.0;
-
-
-包含 1 列和 3854 行的表
+WHERE ratings.rating >= 9.0
+GROUP BY people.id;
--- Week7/movies/9.sql
+++ Week7/movies/9.sql
@@ -1,14 +1,6 @@
-SELECT COUNT(DISTINCT name) FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE movies.year = 2004
-ORDER BY birth;
-
-包含 1 列和 19,325 行的表
-
-SELECT COUNT(name) FROM people
+SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year =2004
-GROUP BY people.id, people.name
+GROUP BY people.id
 ORDER BY birth;
