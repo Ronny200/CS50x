8c6f6daed16b4c3931c147b69cb37f3b59a8b374
ronny <yq.worm@gmail.com>
2024-12-09 14:35:01 +0000

    Invalid DateTime

--- Week7/fiftyville/log.sql
+++ Week7/fiftyville/log.sql
@@ -1,16 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- airports              -- 机场
--- crime_scene_reports   -- 犯罪现场报告
--- people                -- 人员
--- atm_transactions      -- 自动取款机交易
--- flights               -- 航班
--- phone_calls           -- 电话
--- bakery_security_logs  -- 面包店安全日志
--- interviews            -- 采访
--- bank_accounts         -- 银行账户
--- passengers            -- 乘客
-
 -- 查看2023年7月28日Humphrey street的犯罪记录获知鸭子被盗时间为10:15
 SELECT description FROM crime_scene_reports
 WHERE year = 2023 AND month = 7 AND day = 28
@@ -19,7 +8,8 @@ AND street = 'Humphrey Street' AND description LIKE '%CS50%';
 /** 根据采访记录可以判断：
     1. 小偷在盗窃的10分钟内从面包店停车场上车也就是 28 日 10:15 -- 10:25 阶段走的
     2. 小偷在28日早上去Leggett Street街道的银行提款机取过钱
-    3. 小偷在离开面包店的时候打了电话，通话时间不到一分钟。在电话中，我听到小偷说他们计划明天乘坐最早一班从 Fiftyville 出发的航班离开。然后小偷让电话另一端的人帮他们购买机票。
+    3. 小偷在离开面包店的时候打了电话，通话时间不到一分钟并计划明天乘坐
+       最早一班从 Fiftyville 出发的航班离开。然后小偷让电话另一端的人帮他购买机票。
 */
 SELECT transcript FROM interviews
 WHERE year = 2023 AND month = 7 AND day = 28
