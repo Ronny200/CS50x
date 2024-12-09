cb0ad93713fa486b9e053ae40cff49424ba2b074
ronny <yq.worm@gmail.com>
2024-12-09 05:42:41 +0000

    Invalid DateTime

--- Week7/movies/10.sql
+++ Week7/movies/10.sql
@@ -3,3 +3,6 @@ JOIN directors ON people.id = directors.person_id
 JOIN movies ON directors.movie_id = movies.id
 JOIN ratings ON movies.id = ratings.movie_id
 WHERE ratings.rating >= 9.0;
+
+
+包含 1 列和 3854 行的表
--- Week7/movies/9.sql
+++ Week7/movies/9.sql
@@ -3,3 +3,6 @@ JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year = 2004
 ORDER BY birth;
+
+包含 1 列和 19,325 行的表
+
