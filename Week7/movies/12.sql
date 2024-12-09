c9e0552b831052e29261c68138dd9669fda8f6a7
ronny <yq.worm@gmail.com>
2024-12-09 05:41:31 +0000

    Invalid DateTime

--- Week7/movies/12.sql
+++ Week7/movies/12.sql
@@ -1,11 +1,3 @@
--- SELECT title FROM movies
--- JOIN stars s1 ON movies.id = s1.movie_id
--- JOIN stars s2 ON movies.id = s2.movie_id
--- JOIN people p1 ON s1.person_id = p1.id
--- JOIN people p2 ON s2.person_id = p2.id
--- WHERE p1.name = 'Jennifer Lawrence'
--- AND p2.name = 'Bradley Cooper';
-
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
