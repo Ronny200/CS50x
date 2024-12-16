ecbaa809c995de6aa74db34fee9f2e640691381f
ronny <yq.worm@gmail.com>
2024-12-16 18:29:28 +0000

    Invalid DateTime

--- Week9/birthdays/app.py
+++ Week9/birthdays/app.py
@@ -35,6 +35,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
+        people = db.excute("SELECT ")
 
         return render_template("index.html")
 
