adf929a7f346ae91110e3e4feb6eaf80bdc79878
ronny <yq.worm@gmail.com>
2024-12-16 19:37:13 +0000

    Invalid DateTime

--- Week9/birthdays/app.py
+++ Week9/birthdays/app.py
@@ -33,7 +33,7 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
         if not name or not month or not day:
-           return redirect("/")
+            return redirect("/")
         else:
             db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
         return redirect("/")
