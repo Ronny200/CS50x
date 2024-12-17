63ef2104c5542140acecec5a98358df7528d417f
ronny <yq.worm@gmail.com>
2024-12-17 18:01:04 +0000

    Invalid DateTime

--- Week9/finance/app.py
+++ Week9/finance/app.py
@@ -114,7 +114,12 @@ def register():
     """Register user"""
     session.clear()
     if request.method == "POST":
-        
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
 
 
     return apology("TODO")
