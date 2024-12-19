54b69d166a20c2ed67e567f41d9a8de4d23f1bf9
ronny <yq.worm@gmail.com>
2024-12-19 11:24:47 +0000

    Invalid DateTime

--- Week9/finance/app.py
+++ Week9/finance/app.py
@@ -37,8 +37,9 @@ def index():
     """Show portfolio of stocks"""
     user_id = session["user_id"]
     user_cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
+    usd_cash = usd(user_cash["cash"])
     user_shares = db.execute("SELECT * FROM shares WHERE user_id = ?", user_id)
-    return render_template("index.html", user_cash = user_cash, shares = user_shares)
+    return render_template("index.html", user_cash = usd_cash, shares = user_shares)
 
 
 @app.route("/buy", methods=["GET", "POST"])
